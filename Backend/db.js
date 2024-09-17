08e0090583c15461a0209a822f105e9fcb9e86ce
Mohammed Ismail <mohammedismail72425@gmail.com>
2024-09-18 01:16:53 +0530

    Added db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://admin:smile72425@cluster0.el80i.mongodb.net/todo-database")
+mongoose.connect("mongodb+srv://admin:smile72425@cluster0.el80i.mongodb.net/todos-database")
 .then(() => console.log("Connected to mongodb successfully!"))
 .catch(() => console.log("Failed to connect to mongodb!"))
 
@@ -10,6 +10,8 @@ const todoSchema = mongoose.Schema({
     completed: Boolean
 })
 
-const Todos = mongoose.model(todos, todoSchema)
+const Todos = mongoose.model('todos', todoSchema)
 
-module.exports = Todos
+module.exports = {
+    Todos
+}
