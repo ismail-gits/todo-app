d1ea5393e9f3681a1c090a82dc43435df0fec980
Mohammed Ismail <mohammedismail72425@gmail.com>
2024-09-18 01:35:15 +0530

    Update index.js for mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,50 @@
 const express = require('express')
 const { createTodoSchema, updateTodoSchema} = require('./types')
-const Todo = require('./database/index')
+const { Todos } = require('./db')
 
 const app = express()
 
 app.use(express.json())
 
-app.post('/todos', (req, res) => {
+app.post('/todos', async (req, res) => {
+    // Input validation using zod
     const inputValidation = createTodoSchema.safeParse(req.body)
 
     if (!inputValidation.success) {
         return res.status(400).json({message: "Invalid inputs"})
     }
 
-    // push data into mongodb
+    const { title, description} = req.body
+
+    // pushing data to mongodb
+    await Todos.create({
+        title,
+        description,
+        completed: false
+    })
+
+    res.json({message: "Todo created"})
 })
 
-app.get('/todos', (req, res) => {
+app.get('/todos', async (req, res) => {
+    const todos = await Todos.find()
 
+    res.json({todos})
 })
 
-app.put('/completed', (req, res) => {
+app.put('/completed', async (req, res) => {
+    // Input validation using zod
     const inputValidation = updateTodoSchema.safeParse(req.body)
 
     if (!inputValidation.success) {
         return res.status(400).json({message: "Invalid inputs"})
     }
+
+    // updating todo on mongodb
+    await Todos.update(
+        {_id: req.body.id},
+        {completed: true}
+    )
+
+    res.json({message: "Todo marked as completed"})
 })
